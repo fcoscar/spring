877d4507c9698cd2a588e02900f85a43acfa1773
fcoscar <oscarjr01.13@gmaill.com>
2021-06-19 12:44:03 -0400

    Bcrypt

--- src/main/java/com/example/webapp/codificador/Bcrypt.java
+++ src/main/java/com/example/webapp/codificador/Bcrypt.java
@@ -4,6 +4,16 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class Bcrypt {
 
+    public static void main(String ars[]){
+
+        String clave = "1234";
+        System.out.println(encriptar(clave));
+    }
+    public static String encriptar(String clave){
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder.encode(clave);
+    }
+
 }
 
 
